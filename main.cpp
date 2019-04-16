942a4b2a4efa030e817df5420de9e1894f372234
colin casazza <colanzio5@gmail.com>
2019-04-15 19:30:40 -0700

    okay, heres some boiler plate code for real this time

--- main.cpp
+++ main.cpp
@@ -7,84 +7,90 @@
  */
 #include "mizzo.h"
 
-int buffer[BUFFER_SIZE] = {0};
-int index = 0;
-sem_t full, empty;
-pthread_mutex_t mutex;
 
-void *fish_produce(void *arg)
+void *fish_produce(void *ctx)
 {
+    CTX* context = (CTX*)ctx;
     while (1)
     {
         sleep(1);
-        sem_wait(&empty);
-        pthread_mutex_lock(&mutex);
+        sem_wait(&context->empty);
+        pthread_mutex_lock(&context->mutex);
         int item = 1;
-        buffer[index++] = 1;
+        context->index = context->index +1;
+        context->buffer[context->index] = 1;
         std::cout << "Produced Fish" << std::endl;
-        pthread_mutex_unlock(&mutex);
-        sem_post(&full);
+        pthread_mutex_unlock(&context->mutex);
+        sem_post(&context->full);
     }
 }
 
-void *escargot_produce(void *arg)
+void *escargot_produce(void *ctx)
 {
+    CTX* context = (CTX*)ctx;
     while (1)
     {
         sleep(1);
-        sem_wait(&empty);
-        pthread_mutex_lock(&mutex);
-        buffer[index++] = 2;
+        sem_wait(&context->empty);
+        pthread_mutex_lock(&context->mutex);
+        context->index = context->index +1;
+        context->buffer[context->index] = 2;
         std::cout << "Produced Escargot" << std::endl;
-        pthread_mutex_unlock(&mutex);
-        sem_post(&full);
+        pthread_mutex_unlock(&context->mutex);
+        sem_post(&context->full);
     }
 }
 
-void *ethel_consume(void *arg)
+void *ethel_consume(void *ctx)
 {
+    CTX* context = (CTX*)ctx;
     while (1)
     {
         sleep(1);
-        sem_wait(&full);
-        pthread_mutex_lock(&mutex);
-        int item = buffer[--index];
+        sem_wait(&context->full);
+        pthread_mutex_lock(&context->mutex);
+        context->index = context->index -1;
+        int item = context->buffer[context->index];
         std::cout << "Ethel Consumed " << item << std::endl;
-        pthread_mutex_unlock(&mutex);
-        sem_post(&empty);
+        pthread_mutex_unlock(&context->mutex);
+        sem_post(&context->empty);
     }
 }
 
-void *lucy_consume(void *arg)
+void *lucy_consume(void *ctx)
 {
+    CTX* context = (CTX*)ctx;
     while (1)
     {
         sleep(1);
-        sem_wait(&full);
-        pthread_mutex_lock(&mutex);
-        int item = buffer[--index];
+        sem_wait(&context->full);
+        pthread_mutex_lock(&context->mutex);
+        context->index = context->index -1;
+        int item = context->buffer[context->index];
         std::cout << "Lucy Consumed " << item << std::endl;
-        pthread_mutex_unlock(&mutex);
-        sem_post(&empty);
+        pthread_mutex_unlock(&context->mutex);
+        sem_post(&context->empty);
     }
 }
 
 int main()
 {
     
-    // init data
+    // init thread ids and shared data
     pthread_t fish_producer, escargot_producer, ethel_consumer, lucy_consumer;
-    sem_init(&empty, 0, BUFFER_SIZE);
-    sem_init(&full, 0, 0);
-    pthread_mutex_init(&mutex, NULL);
+    CTX context;
+    sem_init(&context.empty, 0, BUFFER_SIZE);
+    sem_init(&context.full, 0, 0);
+    pthread_mutex_init(&context.mutex, NULL);
 
-    // create the producers
-    pthread_create(&fish_producer, NULL, fish_produce, NULL);
-    pthread_create(&escargot_producer, NULL, escargot_produce, NULL);
+    // create the producer threads
+    pthread_create(&fish_producer, NULL, fish_produce, &context);
+    pthread_create(&escargot_producer, NULL, escargot_produce, &context);
 
-    // create the consumers
-    pthread_create(&ethel_consumer, NULL, ethel_consume, NULL);
-    pthread_create(&lucy_consumer, NULL, lucy_consume, NULL);
+    // create the consumer threads
+    pthread_create(&ethel_consumer, NULL, ethel_consume, &context);
+    pthread_create(&lucy_consumer, NULL, lucy_consume, &context);
 
+    // kill all the threads
     pthread_exit(NULL);
 }
--- mizzo.h
+++ mizzo.h
@@ -1,19 +1,29 @@
+
+#ifndef MIZZO_H
+#define MIZZO_H
+
+// librarys used
 #include <iostream>
 #include <pthread.h>
 #include <semaphore.h>
 #include <random>
 #include <unistd.h>
 
-#ifndef MIZZO_H
-#define MIZZO_H
-
+// constants
 #define BUFFER_SIZE 10
 
+// main application functiin
+int mizzo();
+
+// producer functins
 void *fish_produce(void *arg);
 void *escargot_produce(void *arg);
+
+// consumer functions
 void *ethel_consume(void *arg);
 void *lucy_consume(void *arg);
 
+// shared data between consumers and producers
 typedef struct CTX {
     int buffer[BUFFER_SIZE] = {0};
     int index = 0;
